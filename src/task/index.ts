cfad3c4f4fc563f9e229052ca745365a919c1c3a
Ewan Burns <ewan@beezup.com>
2019-02-22 14:40:52 +0100

    Removing ExitTask

--- src/task/index.ts
+++ src/task/index.ts
@@ -6,4 +6,3 @@ export { BaseTask } from "./bases/basetask";
 export { TaskChain } from "./tasks/taskchain";
 export { RetriableTask } from "./tasks/retriabletask";
 export { ApplicationTask } from "./tasks/applicationtask";
-export { ExitTask } from "./tasks/exittask";
